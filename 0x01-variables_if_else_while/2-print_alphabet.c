aa3f91041db2b54bafddfb46e6acf907b5d2f64c
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-17 15:35:38 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Jumana Emad
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int n = 123;
+	int i;
+	for (i = 97; i < n; i++)
+    		putchar(i);
+
+  	putchar('\n');
+
+	return (0);
+}
