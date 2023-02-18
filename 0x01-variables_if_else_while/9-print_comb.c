f8791a52ad9b71b93387c151741ccb9fd05d471d
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-17 16:29:17 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Jumana Emad
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints the numbers followed by , then space
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
