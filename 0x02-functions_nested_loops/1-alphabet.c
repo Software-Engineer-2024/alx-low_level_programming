408841ef05a76353e84277d1f31493f8663b6d48
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 14:25:38 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Jumana Emad
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet.
+ * main -  prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int n = 123;
+	int i;
+
+	for (i = 97; i < n; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
