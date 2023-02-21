2dedca3597064c5670139e3b88e206263f4e2d70
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 15:01:43 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-islower.c
+ * Auth: Jumana Emad
+ */
+
+#include "main.h"
+
+/**
+ * _islower - checks if the int given is lower case;
+ *
+ * Return: 1 if the integer is lower case , 0 otherwise 
+ */
+
+int _islower(int c)
+{
+	int n = 123;
+	int i;
+
+	for (i = 97; i < n; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+	
+	return (0);
+}
