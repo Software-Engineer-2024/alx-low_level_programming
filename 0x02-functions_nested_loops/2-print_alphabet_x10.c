b23d2aa456e2e76c1f2d36258b0e8b32217efb67
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 14:41:24 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 /*
- * File: 1-alphabet.c
+ * File: 2-print_alphabet_x10.c
  * Auth: Jumana Emad
  */
 
 #include "main.h"
 
 /**
- * print_alphabet - print the alphabet.
+ * print_alphabet_x10 - print the alphabet 10 times.
  * main -  prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0
@@ -19,7 +19,7 @@ void print_alphabet_x10(void)
 	int j;
 	
 	for(j = 0; j < 10; j++)
-	{ 
+	{
 		for (i = 97; i < n; i++)
 			_putchar(i);
 
