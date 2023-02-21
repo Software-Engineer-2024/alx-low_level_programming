ce5ceb8176d2a28cfd20b4dc4138b87547912208
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 13:54:26 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,10 +14,10 @@
 int main(void)
 {
 	char str[] = "_putchar";
-        int i;
+	int i;
 
-        for (i = 0 ; i < 8 ; i++ )
-                _putchar(str[i]);
+	for (i = 0 ; i < 8 ; i++ )
+		_putchar(str[i]);
 
         _putchar('\n');
 
