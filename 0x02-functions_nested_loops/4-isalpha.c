b2fe895497b1e9d291d061f8cc8252d05b202610
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 15:17:34 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Jumana Emad
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha - checks if the int given is upper case;
+ * @c : input from user
+ *
+ * Return: 1 if the integer is lower case , 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	int n = 91;
+	int i;
+	int j;
+
+	for (i = 65; i < n; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+
+	for (j = 97; j < 123;j++)
+	{
+		if (c == j)
+			return (1);
+	}
+
+	return (0);
+}
