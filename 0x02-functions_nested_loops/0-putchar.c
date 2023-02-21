29be1297b8bfb41761095876db8215c6d2b7df37
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 13:21:10 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Jumana Emad
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
