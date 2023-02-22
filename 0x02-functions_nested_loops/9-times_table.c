ea4708416cb25d8f4b099be709f49ca9e714714a
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-21 16:24:51 -0800

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+
+#include "main.h"
+
+/**
+ * times_table - print the alphabet 10 times.
+ * main -  prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = (i * j);
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
+int main(void)
+{
+    times_table();
+    return (0);
+}
