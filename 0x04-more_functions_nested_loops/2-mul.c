8232365ec40912fc8a4c884c96c8bb8418078f09
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-23 15:21:21 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - add two given  numbers.
+ *
+ * @a: first number
+ * @b: second number
+ * Return: Always the product.
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
