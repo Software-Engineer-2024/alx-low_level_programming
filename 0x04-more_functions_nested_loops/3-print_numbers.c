a11ace89f4372d0d34495550d708113074f5813f
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-23 15:27:39 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{	
+	int n = 58;
+	int i;
+
+	for (i = 48; i < n; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+}
