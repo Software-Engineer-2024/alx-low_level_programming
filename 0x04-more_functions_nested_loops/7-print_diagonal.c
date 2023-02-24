c25c35ca31772f4c2bdbdee610e3617cbd6c7877
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-23 16:55:51 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print a number of slashes creating a slanted line
+ *@n : number of slashes
+ *Return : void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+		if (i+1 < n)
+		{
+		_putchar('\n');
+		for (j = 0 ; j <= i ; j++)
+		_putchar(32);
+	
+		}
+	}	
+	_putchar('\n');
+}
