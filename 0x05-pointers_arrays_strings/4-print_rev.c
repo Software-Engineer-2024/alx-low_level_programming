cda9074bc645600addba7c90d062251b49a19c5b
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-27 13:43:05 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints string
+ * @s: start of a string
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	char *str ;
+
+	str = s;
+	while (*s != '\0')
+	{
+		s++;
+	}
+	while (s != str)
+	{
+		s--;
+		_putchar(*s);
+
+	}	
+	_putchar('\n');
+}
