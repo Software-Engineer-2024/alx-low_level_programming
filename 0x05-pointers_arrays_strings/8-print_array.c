9bf469b4bcd13817640ace0c08f6432f89e42e74
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-27 15:29:49 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,18 +3,19 @@
 
 /**
  * print_array - prints array elements separated by comma
- * @str: start of a string
+ * @a: start of array
+ * @n: array size
  * Return: void.
  */
 void print_array(int *a, int n)
 {
 	int index;
 
-	for(index = 0; index < n; index++)
+	for (index = 0; index < n; index++)
 	{
-		printf("%d",a[index]);
-		if(index != (n - 1))
-		{	
+		printf("%d", a[index]);
+		if (index != (n - 1))
+		{
 		printf(", ");
 		}
 
