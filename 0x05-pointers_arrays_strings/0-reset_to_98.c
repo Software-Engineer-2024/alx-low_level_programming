80426fe93bc8f8e1d87f125e398d8c5d1ca52d63
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-27 12:04:34 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - change the value of the the given to 98
+ * @n : given by user
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
+
