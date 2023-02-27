a2e8cf7a1c6e618f78f23d1602fc1bd3def1e57e
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-27 15:13:48 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of string
+ * @str: start of a string
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int length, index;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for (index = ((length - 1) / 2) + 1; index <= length; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
