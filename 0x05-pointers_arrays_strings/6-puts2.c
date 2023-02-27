b67c0e59f151c72db702fc759148effdd5038897
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-02-27 14:54:43 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints string
+ * @str: start of a string
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int index;
+
+	index = 0;
+	while (*str != '\0')
+	{
+		if (index%2 == 0)
+		{	
+		_putchar(*str);
+		}
+		index++;
+		str++;
+		
+	}
+	_putchar('\n');
+}
