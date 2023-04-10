be5f74efc1b314115cf3de2fba76698900b4f4b9
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-04-10 16:56:31 -0700

    betty fixed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -16,5 +16,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	else if ((n & (1 << index)) == 0)
 		return (0);
 
-	else return (1);
+	else
+		return (1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * set_bit - sets the value of a certain bit to 1.
- * 
+ *
  * @n: unsigned long int.
  * @index: index of the bit.
  *
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	else 
+	else
 		*n ^= (1 << index);
 
 	return (1);
