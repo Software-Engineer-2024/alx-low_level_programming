ecd1fa32bf95361ac060128b84c197688268fc5f
Software-Engineer-2024 <jumana_emad@yahoo.com>
2023-04-10 17:29:27 -0700

    task 0 and 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters inside
+ * @filename: filename duh.
+ * @letters: numbers of letters to be printed.
+ *
+ * Return: numbers of letters printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	nread = read(fd, buffer, letters);
+	nwrite = write(STDOUT_FILENO, buffer, nread);
+
+	close(fd);
+
+	free(buffer);
+
+	return (nwrite);
+}
